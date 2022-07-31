8ac9a26eaa5a874ad3493a749058898f9b6c8128
svloc <svloc39@gmail.com>
2022-07-31 14:28:14 +0530

    palindrom

---
+++ Number/isPalindrom.js
@@ -0,0 +1,31 @@
+// function isP(n){
+//     let rem;
+//     let rev=0;
+//     let n1=n;
+//     while(n!=0){
+//         rem=n%10;
+//         rev=rev*10+rem;
+//         n=n/10;
+//     }
+//     if(n1==rev)
+//     console.log("Yes");
+//     else
+//     console.log("No");
+
+// }
+
+function isP(num){
+    let arr=String(num).split("");
+    let n =arr.length;
+    let ans=[];
+    for(let i=n-1;i>=0;i--){
+       ans.push(arr[i]);
+    }
+
+    if(num==ans.join(""))
+    console.log("Yes");
+    else
+    console.log("No");
+
+}
+isP(1214);
